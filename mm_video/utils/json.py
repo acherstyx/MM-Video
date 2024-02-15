1d0f52f807eac02e84dcc75ca1be909c3dcc9482
acherstyx <acherstyx@gmail.com>
2024-02-15 13:43:54 +0800

    Add jsonl reader and writer, update config for multirun

--- mm_video/utils/json.py
+++ mm_video/utils/json.py
@@ -6,6 +6,7 @@
 
 import json
 from typing import Union, List, Dict
+from xopen import xopen
 
 
 def load_json(file_path: str):
@@ -26,3 +27,35 @@ def save_json(data: Union[List, Dict], filename: str, save_pretty: bool = False,
             f.write(json.dumps(data, cls=MyEncoder, indent=4, sort_keys=sort_keys))
         else:
             json.dump(data, f)
+
+
+class JsonlReader:
+    def __init__(self, file: str):
+        self.jsonl_file = xopen(file, "r")
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        line = self.jsonl_file.readline()
+        if line:
+            return json.loads(line)
+        else:
+            raise StopIteration
+
+    def to_list(self):
+        return list(iter(self))
+
+
+class JsonlWriter:
+    def __init__(self, file: str):
+        self.jsonl_file = xopen(file, "a")
+
+    def write(self, data, **kwargs):
+        self.jsonl_file.write(json.dumps(data, **kwargs) + "\n")
+
+    def flush(self):
+        self.jsonl_file.flush()
+
+    def close(self):
+        self.jsonl_file.close()
