7c2bd8998846baf641fdaf7bc639451464f3cd7b
acherstyx <acherstyx@gmail.com>
2024-03-01 20:56:53 +0800

    Update json utils

--- mm_video/utils/json.py
+++ mm_video/utils/json.py
@@ -14,24 +14,23 @@ def load_json(file_path: str):
         return json.load(f)
 
 
-def save_json(data: Union[List, Dict], filename: str, save_pretty: bool = False, sort_keys: bool = False):
-    class MyEncoder(json.JSONEncoder):
-
-        def default(self, obj):
-            if isinstance(obj, bytes):  # bytes->str
-                return str(obj, encoding='utf-8')
-            return json.JSONEncoder.default(self, obj)
-
+def save_json(data: Union[List, Dict], filename: str, save_pretty: bool = False, **kwargs):
     with open(filename, "w") as f:
         if save_pretty:
-            f.write(json.dumps(data, cls=MyEncoder, indent=4, sort_keys=sort_keys))
+            f.write(json.dumps(data, indent=4, **kwargs))
         else:
-            json.dump(data, f)
+            json.dump(data, f, **kwargs)
 
 
 class JsonlReader:
-    def __init__(self, file: str):
+    def __init__(self, file: str, auto_reset: bool = False):
+        """
+
+        :param file:
+        :param auto_reset: Whether to automatically reset the file pointer to the beginning when the end is reached.
+        """
         self.jsonl_file = xopen(file, "r")
+        self._auto_reset = auto_reset
 
     def __iter__(self):
         return self
@@ -41,10 +40,21 @@ class JsonlReader:
         if line:
             return json.loads(line)
         else:
+            if self._auto_reset:
+                self.jsonl_file.seek(0)
             raise StopIteration
 
     def to_list(self):
         return list(iter(self))
+
+    def close(self):
+        return self.jsonl_file.close()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
 
 
 class JsonlWriter:
@@ -59,3 +69,9 @@ class JsonlWriter:
 
     def close(self):
         self.jsonl_file.close()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
